535416da3f827aa41f442f0e436cd2a6c6bc1c2a
Simon Charters <simon259@hotmail.co.uk>
2020-12-14 12:24:38 +0000

    can now register new trainers

--- controllers/trainers_controller.py
+++ controllers/trainers_controller.py
@@ -14,3 +14,11 @@ def trainers():
 def new_trainer():
     return render_template("/trainers/new.html")
 
+@trainers_blueprint.route("/trainers", methods=['POST'])
+def register_trainer():
+    name = request.form['name']
+    number = request.form['number']
+    trainer = Trainer(name, number)
+    trainer_repository.save(trainer)
+    return redirect('/trainers')
+
